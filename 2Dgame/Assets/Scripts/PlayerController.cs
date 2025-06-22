a341218eff128f489a7d6d4852edea2fdbbf068e
302596555 <302596555@student.rocmondriaan.nl>
2025-06-22 16:11:22 +0200

    update

--- 2Dgame/Assets/Scripts/PlayerController.cs
+++ 2Dgame/Assets/Scripts/PlayerController.cs
@@ -2,31 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
     private Animator anim;
-    private enum State { idle, run, jump, falling };
+
+    private enum State { idle, run, jump, falling, hurt };
     private State state = State.idle;
+
     private Collider2D coll;
+
     [SerializeField] private LayerMask Ground;
-    [SerializeField] private float speed = 5f;
+    [SerializeField] private float speed = 10f;
     [SerializeField] private float jumpForce = 10f;
     [SerializeField] private int apples = 0;
     [SerializeField] private Text applesText;
-
-
-
+    [SerializeField] private float hurtForce = 10f;
+    [SerializeField] private int health;
+    [SerializeField] private Text healthAmount;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         coll = GetComponent<Collider2D>();
+        healthAmount.text = health.ToString(); 
     }
 
     void Update()
+    {
+        if (state != State.hurt)
+        {
+            Movement();
+        }
+
+        VelocityState();
+        anim.SetInteger("State", (int)state);
+    }
+
+    private void Movement()
     {
         float hDirection = Input.GetAxis("Horizontal");
 
@@ -46,22 +62,8 @@ public class PlayerController : MonoBehaviour
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
             state = State.jump;
         }
-
-        VelocityState();
-        anim.SetInteger("State", (int)state); // Ensure Animator has an int parameter named "state"
     }
 
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.tag == "Collectable")
-        {
-            Destroy(collision.gameObject);
-            apples += 1;
-
-            applesText.text = apples.ToString();
-        }
-    }
     private void VelocityState()
     {
         if (state == State.jump)
@@ -78,7 +80,6 @@ public class PlayerController : MonoBehaviour
                 state = State.idle;
             }
         }
-
         else if (Mathf.Abs(rb.linearVelocity.x) > 2f)
         {
             state = State.run;
@@ -86,6 +87,52 @@ public class PlayerController : MonoBehaviour
         else
         {
             state = State.idle;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Collectable")
+        {
+            Destroy(collision.gameObject);
+            apples += 1;
+            applesText.text = apples.ToString();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            if (state == State.falling)
+            {
+                Destroy(other.gameObject);
+            }
+            else
+            {
+                state = State.hurt;
+
+               HandleHealth();
+
+                if (other.gameObject.transform.position.x > transform.position.x)
+                {
+                    rb.linearVelocity = new Vector2(-hurtForce, rb.linearVelocity.y);
+                }
+                else
+                {
+                    rb.linearVelocity = new Vector2(hurtForce, rb.linearVelocity.y);
+                }
+            }
+        }
+    }
+
+    private void HandleHealth()
+    {
+        health -= 1;
+        healthAmount.text = health.ToString();
+        if (health <= 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
     }
 }
